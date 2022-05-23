7f3ced3cf5e65564fb1dbbfff90c023a5ee1e9a7
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-05-23 18:13:48 -0300

    feat: route to get weather from api

---
+++ exceptions/HttpError.js
@@ -0,0 +1,13 @@
+class HttpError extends Error {
+  constructor(message, status) {
+    super(message);
+
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this, this.constructor);
+
+    this.status = status;
+  }
+}
+
+module.exports = HttpError;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const needle = require('needle');
+const HttpError = require('../exceptions/HttpError');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get('/weather', async (req, res) => {
+  try {
+    const { location, units, language } = req.query;
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      q: location,
+      units: units || 'metric',
+      lang: language || 'pt_br'
+    });
+
+    const response = await needle('get', `${API_BASE_URL}?${params}`);
+
+    if (response.statusCode !== 200) {
+      throw new HttpError(response.body.message, response.statusCode);
+    }
+
+    const data = response.body;
+
+    res.status(200).json(data);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return res.status(error.status).json({
+        message: error.message
+      });
+    }
+
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
