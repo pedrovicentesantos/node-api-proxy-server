49854573fe1745e5286ea9b0081929df38a3bba2
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-05-23 18:20:28 -0300

    feat: add rate limiting

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -7,6 +9,20 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  legacyHeaders: false,
+  handler: (_, res) => {
+    res.status(429).json({
+      message: 'Too many requests, please try again later.'
+    });
+  }
+});
+
+app.use(limiter);
+app.set('trust proxy', 1);
 
 app.use('/api/v1', require('./routes'));
 
