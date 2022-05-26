c0bb6c29cc3c1cd685ca9cc4e5abc8b28511cd0a
Pedro Vicente Santos <pedrovicente.dev@gmail.com>
2022-05-25 21:49:47 -0300

    feat: add css styles for weather information

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 const form = document.querySelector('form');
 
+const roundNumber = (num, decimalPlaces = 1) => {
+  const factor = Math.pow(10, decimalPlaces);
+  return Math.round(num * factor) / factor;
+};
+
 const getWeather = async location => {
   const response = await fetch(`/api/v1/weather?location=${location}`);
 
@@ -19,17 +24,44 @@ form.addEventListener('submit', async e => {
   try {
     e.preventDefault();
 
+    result.innerHTML = 'Loading...';
+
     const location = document.querySelector('input').value;
     const response = await getWeather(location);
 
     result.innerHTML = `
-      <div>
-        <h5 class="white-text">${response.weather[0].description}</h5>
-        <p class="white-text">${response.main.temp}</p>
+      <p class="font-bold text-xl tex-stone-700">${response.name}</p>
+      <div class="grid grid-rows-2 grid-cols-2 mt-8">
+          <div class="grid">
+            <img
+              src=
+              "http://openweathermap.org/img/w/${response.weather[0].icon}.png"
+              alt="${response.weather[0].description}"
+            />
+            <h5 class="capitalize text-stone-600 justify-self-start">
+              ${response.weather[0].description}
+            </h5>
+          </div>
+          <div class="grid justify-items-center content-between justify-self-end">
+            <h3 class="text-stone-800 text-3xl justify-self-end">
+              ${roundNumber(response.main.temp)}&deg;C
+            </h3>
+            <p class="text-stone-600">
+              Sensação de ${roundNumber(response.main.feels_like)}&deg;C
+            </p>
+          </div>
+        <div class="flex row-start-2 col-start-2 justify-self-end font-semibold">
+          <h5 class="text-red-700">
+            ${roundNumber(response.main.temp_max)}&deg;
+          </h5>
+          <h5 class="text-gray-400">/</h5>
+          <h5 class="text-blue-500">
+            ${roundNumber(response.main.temp_min)}&deg;
+          </h5>
+        </div>
       </div>
     `;
   } catch (error) {
-    console.log(error.message);
-    result.innerHTML = `<p>${error.message}</p>`;
+    result.innerHTML = `<p class="text-red-600">${error.message}</p>`;
   }
 });
